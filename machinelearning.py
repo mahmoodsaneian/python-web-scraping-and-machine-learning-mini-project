0cefe501e5a06a97b8cac7b26f169621b84e75a0
mahmoodsaneian <mahmoodsaneian1@gmail.com>
2021-09-17 18:24:22 +0430

    give data to computer

--- machinelearning.py
+++ machinelearning.py
@@ -1,10 +1,18 @@
 from sklearn import tree
 import mysql.connector
 
+x = []
+y = []
+
 # connect to the database
 cnx = mysql.connector.connect(user = 'root', password = '', database = 'python_project', host = '127.0.0.1')
 cursor = cnx.cursor()
 # read from database
-cursor.execute('SELECT name, functionality, accident, year FROM car')
+cursor.execute('SELECT name, functionality, accident, year, price FROM car')
 res = cursor.fetchall()
+
+for i in range(0, len(res)) :
+    tmp = list(res[i])
+    x.append(tmp[0:4])
+    y.append(tmp[4])
 
